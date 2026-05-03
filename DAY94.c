3ba241861a609ca92b1a2f66e45ee0d99bd288e6
NILAY SHARMA <grad.nilay@gmail.com>
2026-05-03 23:02:30 +0530

    Add files via upload

---
+++ DAY94.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    int max = arr[0];
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > max)
+            max = arr[i];
+    }
+
+    int *count = (int *)calloc(max + 1, sizeof(int));
+
+    for (int i = 0; i < n; i++)
+        count[arr[i]]++;
+
+    for (int i = 1; i <= max; i++)
+        count[i] += count[i - 1];
+
+    int output[n];
+    for (int i = n - 1; i >= 0; i--) {
+        output[count[arr[i]] - 1] = arr[i];
+        count[arr[i]]--;
+    }
+
+    for (int i = 0; i < n; i++)
+        arr[i] = output[i];
+
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    free(count);
+
+    return 0;
+}
